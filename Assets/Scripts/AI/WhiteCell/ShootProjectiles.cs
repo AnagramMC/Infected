8426cfdec244c3a85b01ca7a0783c76723d8112e
lago2015 <chilenoo92@gmail.com>
2018-01-26 20:59:25 -0500

    Folders added and Shoot Projectiles script created

---
+++ Assets/Scripts/AI/WhiteCell/ShootProjectiles.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootProjectiles : MonoBehaviour {
+
+    public GameObject[] ProjectilePos;
+    public GameObject Projectile;
+    public GameObject Muzzle;
+    public float FireRate;
+    float elapseTime;
+    bool isReloading = false;
+    bool PlayerNear;
+
+    void Awake()
+    {
+        if (Muzzle)
+        {
+            Muzzle.SetActive(false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        LaunchProjectiles();
+    }
+
+    void LaunchProjectiles()
+    {
+        if (PlayerNear)
+        {
+            elapseTime += Time.deltaTime;
+            if (elapseTime >= FireRate)
+            {
+                int PosLength = ProjectilePos.Length;
+                for (int i = 0; i < PosLength; i++)
+                {
+                    if (!isReloading)
+                    {
+                        Instantiate(Projectile, ProjectilePos[i].transform.position, ProjectilePos[i].transform.rotation);
+                        Muzzle.SetActive(true);
+                        StartCoroutine(MuzzleShot());
+                    }
+                }
+                isReloading = true;
+                elapseTime = 0;
+
+            }
+            else
+            {
+
+                isReloading = false;
+            }
+        }
+    }
+
+    IEnumerator MuzzleShot()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Muzzle.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            PlayerNear = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            PlayerNear = false;
+        }
+    }
+}
