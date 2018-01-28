4d0f1d3d8582190c8bfea1928e2a293b5cc3144d
Douglas1701 <matthew@matthewwalterdouglas.com>
2018-01-27 19:25:48 -0500

    Level HUD Work

--- Assets/Scripts/Managers/TimeManager.cs
+++ Assets/Scripts/Managers/TimeManager.cs
@@ -26,7 +26,7 @@ public class TimeManager : MonoBehaviour
     void updateTime()
     {
         seconds += Time.deltaTime;
-        timeText.text = hours + ":" + minutes + ":" + (int)seconds + "s";
+        timeText.text = hours + ":" + minutes + ":" + (int)seconds;
         if (seconds >= 60)
         {
             minutes++;
