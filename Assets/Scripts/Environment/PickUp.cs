6e66f704840aec493c142a7e90c0ba8f1e97b52f
Douglas1701 <matthew@matthewwalterdouglas.com>
2018-01-27 20:15:03 -0500

    Started PickUp

---
+++ Assets/Scripts/Environment/PickUp.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+
+    enum pickUpTypes {LIFE, SHOTGUN, TRISHOT, GRENADE}
+
+    [SerializeField]
+    pickUpTypes pickUp;
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            switch(pickUp)
+            {
+                case pickUpTypes.LIFE:
+                    //Shotgun
+                    break;
+
+                case pickUpTypes.SHOTGUN:
+                    //Tri Shot
+                    break;
+
+                case pickUpTypes.TRISHOT:
+                    // Life
+                    break;
+
+                case pickUpTypes.GRENADE:
+                    //Grenade
+                    break;
+            }
+
+            //Destroy object? or Pool
+        }
+    }
+}
