877fc64faf042c99921ce510c5ea34ab5da3a6c5
lago2015 <chilenoo92@gmail.com>
2018-01-28 00:54:02 -0500

    Background tiling

---
+++ Assets/Scripts/Managers/BackgroundManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundManager : MonoBehaviour {
+
+
+    public GameObject respawnPoint;
+
+    void OnTriggerEnter(Collider collision)
+    {
+        
+        if(collision.gameObject.tag==("Background"))
+        {
+            GameObject curObject = collision.gameObject;
+            curObject.transform.position = respawnPoint.transform.position; 
+            
+        }
+    }
+
+
+}
